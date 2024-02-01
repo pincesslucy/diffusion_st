858a1e54e221ee0a32fcf833279d23a53b455f92
anton0909 <anton0909@naver.com>
2024-02-02 00:38:42 +0900

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   pages/image2image.py

--- pages/image2image.py
+++ pages/image2image.py
@@ -7,33 +7,48 @@ st.set_page_config(
     page_title='i2i'
 )
 
-st.header("그림 그려줌", divider="rainbow")
-
+st.header("그림 그려줌(안써도됨)", divider="rainbow")
 
 content = st.text_input("그린 것 설명(영어로)")
-# 색상 선택기
-stroke_color = st.color_picker("선 색상 선택", '#000000')
-# 선의 너비를 사용자가 선택 가능하도록 슬라이더 추가
-stroke_width = st.slider("굵기 선택", min_value=1, max_value=50, value=2)
 
-# 그림 그리기 캔버스 만들기
-canvas_result = st_canvas(
-    fill_color="rgb(0, 0, 0)",  # 채우기 색상
-    stroke_width=stroke_width,  # 선의 너비
-    stroke_color=stroke_color,  # 사용자가 선택한 선의 색상
-    background_color="#ffffff",  # 배경색
-    height=500,  # 캔버스의 높이
-    width=600,  # 캔버스의 너비
-    drawing_mode="freedraw",  # 그리기 모드
-    key="canvas",
-)
+
+
+# 체크박스 추가
+check = st.checkbox('사진 업로드 기능 사용')
+
+if not check:
+    # 색상 선택기
+    stroke_color = st.color_picker("선 색상 선택", '#000000')
+
+    # 선의 너비를 사용자가 선택 가능하도록 슬라이더 추가
+    stroke_width = st.slider("굵기 선택", min_value=1, max_value=50, value=2)
+    # 그림 그리기 캔버스 만들기
+    canvas_result = st_canvas(
+        fill_color="rgb(0, 0, 0)",  # 채우기 색상
+        stroke_width=stroke_width,  # 선의 너비
+        stroke_color=stroke_color,  # 사용자가 선택한 선의 색상
+        background_color="#ffffff",  # 배경색
+        height=500,  # 캔버스의 높이
+        width=600,  # 캔버스의 너비
+        drawing_mode="freedraw",  # 그리기 모드
+        key="canvas",
+    )
+else:
+    uploaded_file = st.file_uploader("사진 업로드", type=["jpg", "png"])
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded Image.', use_column_width=True)
 
 button = st.button("그림 제출")
 
 if button:
     with st.spinner('그림 그리는중...'):
-        image = Image.fromarray(canvas_result.image_data)
-        image = image.convert("RGB")
+        if not check:
+            image = Image.fromarray(canvas_result.image_data)
+            image = image.convert("RGB")
+        else:
+            image = image.convert("RGB")
+            image = image.resize((512, 512))
         result = generate_i2i(prompt='', image=image)
     st.image(result)
     st.success('완료')
